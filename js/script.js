930fd314de6c58c2b346e8458853d4c9d3f4e310
Vladislav <pisma.drgn@mail.ru>
2020-12-11 17:10:44 +0400

    HomeWork #2

--- js/script.js
+++ js/script.js
@@ -10,12 +10,31 @@ const personalMoveDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+let ask = true;
+let a, b;
 
-personalMoveDB.movies[a] = b;
-personalMoveDB.movies[c] = d;
+do {
+    a = prompt('Один из последних просмотренных фильмов?', '');
+    b = prompt('На сколько оцените его?', '');
+
+    if (a === null || b === null || a === '' || b === '' || a.length > 50) {
+        alert('Ошибка');
+        continue;
+    } else {
+        personalMoveDB.movies[a] = b;
+
+        ask = confirm('Добавить еще фильм?');
+    }
+} while (ask === true);
+
+if (personalMoveDB.count < 10){
+    alert ('Просмотренно довольно мало фильмов');
+} else if (personalMoveDB.count >= 10 || personalMoveDB.count <= 30) {
+    alert ('Вы классный зритель!');
+} else if (personalMoveDB.count > 30) {
+    alert ('Вы киноман');
+} else{
+    alert ('Произошла ошибка');
+}
 
 console.log(personalMoveDB);
