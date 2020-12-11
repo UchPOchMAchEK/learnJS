01f891ae4c10048917622344f56d287ac8dd9f82
Vladislav <pisma.drgn@mail.ru>
2020-12-11 22:26:03 +0400

    HomeWork #3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
 
 const personalMoveDB = {
     count: numberOfFilms,
@@ -10,31 +19,49 @@ const personalMoveDB = {
     privat: false
 };
 
-let ask = true;
-let a, b;
-
-do {
-    a = prompt('Один из последних просмотренных фильмов?', '');
-    b = prompt('На сколько оцените его?', '');
-
-    if (a === null || b === null || a === '' || b === '' || a.length > 50) {
-        alert('Ошибка');
-        continue;
-    } else {
-        personalMoveDB.movies[a] = b;
-
-        ask = confirm('Добавить еще фильм?');
-    }
-} while (ask === true);
-
-if (personalMoveDB.count < 10){
-    alert ('Просмотренно довольно мало фильмов');
-} else if (personalMoveDB.count >= 10 || personalMoveDB.count <= 30) {
-    alert ('Вы классный зритель!');
-} else if (personalMoveDB.count > 30) {
-    alert ('Вы киноман');
-} else{
-    alert ('Произошла ошибка');
+function rememberMyFilms() {
+    let ask = true;
+    let a, b;
+    
+    do {
+        a = prompt('Один из последних просмотренных фильмов?', '');
+        b = prompt('На сколько оцените его?', '');
+    
+        if (a === null || b === null || a === '' || b === '' || a.length > 50) {
+            alert('Ошибка');
+            continue;
+        } else {
+            personalMoveDB.movies[a] = b;
+    
+            ask = confirm('Добавить еще фильм?');
+        }
+    } while (ask === true);
 }
+rememberMyFilms();
 
-console.log(personalMoveDB);
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++ ){
+        personalMoveDB.genres[i - 1] = prompt(`Ващ любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
+
+function detectPersonalLevel() {
+    if (personalMoveDB.count < 10){
+        alert ('Просмотренно довольно мало фильмов');
+    } else if (personalMoveDB.count >= 10 || personalMoveDB.count <= 30) {
+        alert ('Вы классный зритель!');
+    } else if (personalMoveDB.count > 30) {
+        alert ('Вы киноман');
+    } else{
+        alert ('Произошла ошибка');
+    }
+}
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden){
+        console.log(personalMoveDB);
+    }
+}
+showMyDB(personalMoveDB.privat);
